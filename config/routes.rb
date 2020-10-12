2c03c11e5477b43e5e5befcb50f9d6e27e1fff40
NerlyTon <nerlyton@gmail.com>
2020-10-12 14:38:54 -0500

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
+  resources :excursions
+  resources :bookings
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
