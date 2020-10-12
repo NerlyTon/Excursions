d676a287aeb2e9ae2e6560588f712320667937d0
NerlyTon <nerlyton@gmail.com>
2020-10-12 15:51:38 -0500

    added sessions create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(name: params[:user][:name])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to @user
+      else
+        redirect_to 'signin_path'
+      end
   end
 
   def login
