5b19e36360c3051fbb84f5a30496fe42ad82f077
NerlyTon <nerlyton@gmail.com>
2020-10-12 14:36:00 -0500

    added the associations to models

--- app/models/excursion.rb
+++ app/models/excursion.rb
@@ -1,2 +1,4 @@
 class Excursion < ApplicationRecord
+    has_many :bookings
+    has_many :users, through: :bookings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    
+    has_many :bookings
+    has_many :excursions, through: :bookings
+
 end
