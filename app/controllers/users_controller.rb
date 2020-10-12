10b7a0089e9be844f9050da003802f2a7160ac5d
NerlyTon <nerlyton@gmail.com>
2020-10-12 14:59:00 -0500

    added user controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    @user = User.create(params.require(:user).permit(:username,        
-    :password))
-    session[:user_id] = @user.id
-    redirect_to '/welcome'
+    @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        redirect_to new_user_path
+      end
   end
+
+  def show
+    redirect_if_not_logged_in
+    @user = User.find_by(params[:id])
+    redirect_to '/' if !@user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :email, :username)
+  end
+  
 end
