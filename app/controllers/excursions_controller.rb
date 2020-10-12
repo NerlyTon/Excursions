2443c2b4742643692210144cacff1242402a5e48
NerlyTon <nerlyton@gmail.com>
2020-10-12 17:42:26 -0500

    completed the excursion controller actions

--- app/controllers/excursions_controller.rb
+++ app/controllers/excursions_controller.rb
@@ -1,30 +1,42 @@
 class ExcursionsController < ApplicationController
     
     def index
-
+        @excursions = Excursion.all
     end
 
     def new
-
+        @excursion = Excursion.new
     end
 
     def create
-
+        @excursion = Excursion.new(excursion_params)
+        if @excursion.save
+            redirect_to excursion_path(@excursion)
+        else
+            redirect_to new_excursion_path
+        end
     end
 
     def show 
-
+        @excursion = Excursion.find(params[:id])
     end
 
     def edit
-
+        @excursion = Excursion.find(params[:id])
     end
 
     def update
-
+        @excursion = Excursion.find(params[:id])
+        @excursion.update(excursion_params)
+        redirect_to excursion_path(@excursion)
     end
 
     def destroy
         
     end
+
+    private
+
+    def excursion_params
+        params.require(:excursion).permit(:company, :location, :price, :included, :what_to_bring, :description)
 end
