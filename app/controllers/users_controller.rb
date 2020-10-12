eed3128fb36956a7c1df4f068b4a4530ed277859
NerlyTon <nerlyton@gmail.com>
2020-10-12 16:27:54 -0500

    added helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    # redirect_if_not_logged_in
+    redirect_if_not_logged_in
     @user = User.find(params[:id])
     redirect_to '/' if !@user
   end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,19 @@
 module UsersHelper
+    
+    def logged_in?
+        !!session[:user_id]
+    end
+    
+    def current_user
+        @user = User.find_by_id(session[:user_id]) if logged_in?
+    end
+    
+    def redirect_if_not_logged_in
+        redirect_to '/' if !logged_in?
+    end
+
+    def admin?
+        current_user.try(:admin?)
+    end
+    
 end
