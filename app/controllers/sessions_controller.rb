4a1294ca381ec5956ebdf529d8e1d10196604aa5
NerlyTon <nerlyton@gmail.com>
2020-10-12 16:20:39 -0500

    login path works succesfully

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
+    @user = User.find_by(email: params[:user][:email])
       if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
         redirect_to @user
       else
-        redirect_to 'signin_path'
+        redirect_to 'login_path'
       end
-  end
-
-  def login
   end
 
   def welcome
