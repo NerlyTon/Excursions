d4eb4f3b3ec8af5439601f48c83b892483145ab3
NerlyTon <nerlyton@gmail.com>
2020-10-12 13:35:34 -0500

    added html in the views and info in the user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(params.require(:user).permit(:username,        
+    :password))
+    session[:user_id] = @user.id
+    redirect_to '/welcome'
   end
 end
